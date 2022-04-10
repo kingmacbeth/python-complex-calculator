67c54398abf4de377e4dc0d8fce05efc0d797c95
Renato Carneiro <renato.carneiro19@gmail.com>
2022-04-10 10:48:08 -0300

    keep calc running

--- calculator.py
+++ calculator.py
@@ -43,11 +43,6 @@ class Converter():
         self.user_number = user_number
 
     def convert_to_float(self):
-        """Funcao para converter o input do usuario para float aceitando pi ou euler.
-
-        Args:
-            user_number (float): input do usuario que sera passado como tipo float
-        """
         pi_or_euler = {
             "pi": PI,
             "euler": EULER,
@@ -58,60 +53,57 @@ class Converter():
         else:
             return float(self.user_number)
 
-    # def convert_to_pi_or_euler(self):
-    #     if 
-    #         pass
-    #     elif real == EULER:
-    #         pass
+my_bool = True
 
+while my_bool:
+    re1 = input("Digite o numero real do primeiro complexo: ")
+    im1 = input("Digite o numero imaginario do primeiro complexo: ")
 
-re1 = input("Digite o numero real do primeiro complexo: ")
-im1 = input("Digite o numero imaginario do primeiro complexo: ")
+    converter_real = Converter(re1)
+    real = converter_real.convert_to_float()
 
-converter_real = Converter(re1)
-real = converter_real.convert_to_float()
+    converter_imag = Converter(im1)
+    imag = converter_imag.convert_to_float()
 
-converter_imag = Converter(im1)
-imag = converter_imag.convert_to_float()
+    z1 = complex(real, imag)
+    operation = Operations(z1)
 
-z1 = complex(real, imag)
-operation = Operations(z1)
+    print("[1] Conjugado\n[2] Adicao\n[3] Subtracao\n[4] Multiplicacao\n[5] Divisao")
+    escolha = input("Digite qual operacao deseja utilizar: ")
 
-print("[1] Conjugado\n[2] Adicao\n[3] Subtracao\n[4] Multiplicacao\n[5] Divisao")
-escolha = input("Digite qual operacao deseja utilizar: ")
+    if escolha == "1":
+        conjugado = operation.conjugated()
+        print("o conjugado é", conjugado)
 
-if escolha == "1":
-    conjugado = operation.conjugated()
-    print("o conjugado é", conjugado)
+    elif escolha != "1":
+        re2 = input("Digite o numero real do segundo complexo: ")
+        im2 = input("Digite o numero imaginario do segundo complexo: ")
 
-elif escolha != "1":
-    re2 = input("Digite o numero real do segundo complexo: ")
-    im2 = input("Digite o numero imaginario do segundo complexo: ")
+        converter_real_2 = Converter(re2)
+        real_2 = converter_real.convert_to_float()
 
-    converter_real_2 = Converter(re2)
-    real_2 = converter_real.convert_to_float()
+        converter_imag_2 = Converter(im2)
+        imag_2 = converter_imag.convert_to_float()
 
-    converter_imag_2 = Converter(im2)
-    imag_2 = converter_imag.convert_to_float()
+        z2 = complex(real_2, imag_2)
 
-    z2 = complex(real_2, imag_2)
+        if escolha == "2":
+            resultado = operation.addition(z2)
+            print("A soma dos complexos é: ", resultado)
 
-    if escolha == "2":
-        soma = operation.addition(z2)
-        print("A soma dos complexos é: ", soma)
+        elif escolha == "3":
+            resultado = operation.subtraction(z2)
+            print("A subtraçao dos complexos é: ", resultado)
 
-    elif escolha == "3":
-        sub = operation.subtraction(z2)
-        print("A subtraçao dos complexos é: ", sub)
+        elif escolha == "4":
+            resultado = operation.multiplication(z2)
+            print("A multiplicação dos complexos é: ", resultado)
 
-    elif escolha == "4":
-        mult = operation.multiplication(z2)
-        print("A multiplicação dos complexos é: ", mult)
+        elif escolha == "5":
+            resultado = operation.division(z2)
+            print("A divisão dos complexos é: ", resultado)
 
-    elif escolha == "5":
-        div = operation.division(z2)
-        print("A divisão dos complexos é: ", div)
-
-#TODO Adicionar funcao para deixar a calculadora rodando
-#TODO Adicionar opcao de reiniciar a calculadora
-#TODO Adicionar opcao de realizar outra operacao com o resultado final
+    #TODO Adicionar funcao para deixar a calculadora rodando
+    #TODO Adicionar opcao de reiniciar a calculadora
+    #TODO Adicionar opcao de desligar a calculadora
+    #TODO Adicionar opcao de realizar outra operacao com o resultado final
